183f5c4cf7728e7fe34fe1463d96304f39d7ea35
Ibrahim <coder.ibrahimgazi@gmail.com>
2024-02-25 02:04:51 +0600

    Complete Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,14 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
+        {/* Header */}
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <div className="bg-slate-200">
+        <div className="flex justify-between items-center list-none p-3 mx-auto max-w-7xl">
+          <Link to="/">
+            <h1 className="font-bold">Mern App</h1>
+          </Link>
+          <ul className="flex gap-4">
+            <Link to="/">
+              <li>Home</li>
+            </Link>
+            <Link to="/about">
+              <li>About</li>
+            </Link>
+            <Link to="/signin">
+              <li>Sign In</li>
+            </Link>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Header
+export default Header;
